b71a84e1b139b9dfb3c60458148a82d9eee3cb73
John Ratana <jratana1@gmail.com>
2020-12-03 01:02:02 -0500

    Like models created

--- app/models/likes.rb
+++ app/models/likes.rb
@@ -1,4 +1,5 @@
 class Like < ActiveRecord::Base
     belongs_to :restaurant
     belongs_to :user
+    validates :user_id, uniqueness: {scope: :restaurant_id}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :photos
     has_many :likes, dependent: :destroy
-    has_many :liked_restaurants, :through => :likes, :source => :restaurants
+    has_many :liked_restaurants, through: :likes, source: :restaurant
   end
-  
