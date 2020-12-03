0736b359ceb84768283395fd12c024f18ffb1a29
John Ratana <jratana1@gmail.com>
2020-12-03 02:56:52 -0500

    update colors

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20201203051958) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
+    t.string   "name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
